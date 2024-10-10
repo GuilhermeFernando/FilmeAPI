09de3c426eaabf5309317929d2c7d0519312fb6b
Guilherme Machado <guifermach@hotmail.com>
2024-10-10 10:50:14 -0300

    Alterando forma de deletecao para RESTRICT

--- obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da9bf6bde3df3aa068efc35f40450cbab71e18d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a387250169504749c6137e103aa0905da3605cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
